0c0cc1a2dfa20524ae34cba9ab7b632ad78d56d7
Pramod V <pramod@intelops.dev>
2022-08-19 14:30:23 -0400

    Fix shell code smells

--- aws/talos/scripts/bootstrapetcd.sh
+++ aws/talos/scripts/bootstrapetcd.sh
@@ -3,34 +3,34 @@
 
 TALOS_API_IP = 50000
 
-if [ $# -ne 1 ]
+if [ "$#" -ne 1 ]
 then
     echo "Usage: $0 nodeipaddress"
 fi
 
 echo "node ip address is:"
-echo $1
+echo "$1"
 
-remoteip= $1
-count= 0
+remoteip="$1"
+count=0
 
-while [ $count >= 20 ]
+while [ "$count" -ge 20 ]
 do
     echo "Waiting for Talos API to be up...."
-    nc -zv $remoteip $TALOS_API_IP
-    if [ $? -eq 0 ]
+    nc -zv "$remoteip" "$TALOS_API_IP"
+    if [ "$?" -eq 0 ]
     then
         echo "Talos API is up bootstrapping etcd"
-        talosctl  --talosconfig out/talosconfig config endpoint $remoteip
-        talosctl  --talosconfig out/talosconfig config node $remoteip
-        talosctl --talosconfig  out/talosconfig bootstrap $remoteip
+        talosctl  --talosconfig out/talosconfig config endpoint "$remoteip"
+        talosctl  --talosconfig out/talosconfig config node "$remoteip"
+        talosctl --talosconfig  out/talosconfig bootstrap "$remoteip"
         break
     fi
     sleep 30
     count += 1
 done
 
-if [ $count >= 20 ]
+if [ "$count" -ge 20 ]
 then
     echo "ERROR: Talos API is not up "
 fi
