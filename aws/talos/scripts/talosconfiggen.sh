c21fa7778b9060cec4d1be782de4224cd3fd721d
Pramod <pramod@Pramods-MacBook-Pro.local>
2023-02-05 14:00:07 +0530

    Adding openebs

--- aws/talos/scripts/talosconfiggen.sh
+++ aws/talos/scripts/talosconfiggen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]
+if [ $# -ne 2 ]
 then
     echo "Usage: $0 dnsname port"
 fi
@@ -8,7 +8,7 @@ fi
 while getopts "h:p" OPTION;
 do
     case "${OPTION}" in
-    h)  
+    h) 
        dnsname="$OPTARG" 
        ;;
     p) 
@@ -16,29 +16,28 @@ do
        ;;
     esac
 done
-if ! command -v talosctl &> /dev/null
+if ! command -v ./talosctl &> /dev/null
 then
     echo "Installing talos cli"
-    curl -Lo /usr/local/bin/talosctl https://github.com/siderolabs/talos/releases/download/v1.1.1/talosctl-"$(uname -s | tr "[:upper:]" "[:lower:]")"-amd64
-    chmod +x /usr/local/bin/talosctl
+    curl -Lo talosctl https://github.com/siderolabs/talos/releases/download/v1.1.1/talosctl-$(uname -s | tr "[:upper:]" "[:lower:]")-arm64
+    chmod +x ./talosctl
 else
     echo "talosctl is already installed skipping.."
 fi
 
-echo "${dnsname}"
-echo "${4}"
-echo "${port}"
-talosctl gen config talosconfig-userdata https://"${dnsname}":"${4}" --with-examples=false --with-docs=false --output-dir scripts/
-talosctl validate --config scripts/controlplane.yaml --mode cloud
-if [ "$?" -eq 1 ]
+echo ${dnsname}
+echo ${4}
+./talosctl gen config talosconfig-userdata https://${dnsname}:${4} --with-examples=false --with-docs=false --output-dir scripts/ --config-patch @scripts/patch.yaml
+./talosctl validate --config scripts/controlplane.yaml --mode cloud
+if [ $? -eq 1 ]
 then
     echo "scripts/controlplane.yaml is invalid"
     exit
 fi
 
-talosctl validate --config scripts/worker.yaml --mode cloud
+./talosctl validate --config scripts/worker.yaml --mode cloud
 
-if [ "$?" -eq 1 ]
+if [ $? -eq 1 ]
 then
     echo "scripts/worker.yaml is invalid"
     exit
