3fbf78921fdd9e916b0e5be48c20a738897154c2
Pramod <pramod@Pramods-MacBook-Pro.local>
2023-08-30 00:12:00 +0530

    azure installation optimization

--- azure/talos/scripts/bootstrapetcd.sh
+++ azure/talos/scripts/bootstrapetcd.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-TALOS_API_IP = 50000
-
 if [ "$#" -ne 1 ]
 then
     echo "Usage: $0 nodeipaddress"
@@ -14,16 +11,15 @@ echo "$1"
 remoteip="$1"
 count=0
 
-while [ "$count" -ge 20 ]
+while [ "$count" -le 20 ]
 do
     echo "Waiting for Talos API to be up...."
-    nc -zv "$remoteip" "$TALOS_API_IP"
+    nc -zv "$remoteip" 50000
     if [ "$?" -eq 0 ]
     then
         echo "Talos API is up bootstrapping etcd"
-        talosctl  --talosconfig out/talosconfig config endpoint "$remoteip"
-        talosctl  --talosconfig out/talosconfig config node "$remoteip"
-        talosctl --talosconfig  out/talosconfig bootstrap "$remoteip"
+        talosctl  --talosconfig scripts/talosconfig config endpoint "$remoteip"
+        talosctl --talosconfig  scripts/talosconfig bootstrap --nodes "$remoteip"
         break
     fi
     sleep 30
