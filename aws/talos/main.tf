0afcc59b9d7d72d3b46ce6ac823a3e8544d79e9c
vpramod03 <110480433+vpramod03@users.noreply.github.com>
2024-04-27 22:48:46 +0530

    Add scaling for aws

--- aws/talos/main.tf
+++ aws/talos/main.tf
@@ -130,68 +130,110 @@ resource "aws_placement_group" "talosplacemnentgroup" {
   strategy = "cluster"
 }
 
-resource "aws_instance" talos_master_instance {
+resource "aws_launch_configuration" "talosmaster" {
+  name = "talos-master"
+  image_id = data.aws_ami.talos.id
+  instance_type = var.instance_type
+}
 
-    count = var.mastercount
+resource "aws_autoscaling_group" "talosmaster-static" {
+  name                      = "talosmaster-static"
+  max_size                  = 20
+  min_size                  = var.mastercount
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
+  placement_group           = aws_placement_group.talosplacemnentgroup.id
+  launch_configuration      = aws_launch_configuration.talosmaster.name
 
-    ami = data.aws_ami.talos.id
-    instance_type = var.instance_type
-    monitoring  = var.nodemonitoringenabled
-    vpc_security_group_ids = [ module.security_group.security_group_id ]
-    subnet_id              = "${element(module.vpc.private_subnets, 0)}"
 
-    user_data = data.local_file.controllerfile.content
-    associate_public_ip_address = true
-
-    root_block_device {
-       volume_size = 200
-    }
-
-    depends_on = [ data.local_file.controllerfile ]
-
-    tags = {
-       Name = "talosmaster"
-    }
-
+  timeouts {
+    delete = "15m"
+  }
 
 }
-resource "aws_instance" talos_worker_instance {
+
+resource "aws_autoscaling_group" "talosmaster-scalable" {
+  name                      = "talosmaster-scalable"
+  max_size                  = 20
+  min_size                  = var.mastercount
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
+  placement_group           = aws_placement_group.talosplacemnentgroup.id
+  launch_configuration      = aws_launch_configuration.talosmaster.name
+
+
+  timeouts {
+    delete = "15m"
+  }
+
+}
+
+# resource "aws_instance" talos_master_instance {
+
+#     count = var.mastercount
+
+#     ami = data.aws_ami.talos.id
+#     instance_type = var.instance_type
+#     monitoring  = var.nodemonitoringenabled
+#     vpc_security_group_ids = [ module.security_group.security_group_id ]
+#     subnet_id              = "${element(module.vpc.private_subnets, 0)}"
+
+#     user_data = data.local_file.controllerfile.content
+#     associate_public_ip_address = true
+
+#     root_block_device {
+#        volume_size = 200
+#     }
+
+#     depends_on = [ data.local_file.controllerfile ]
+
+#     tags = {
+#        Name = "talosmaster"
+#     }
+
+
+# }
+# resource "aws_instance" talos_worker_instance {
     
-    count = var.workercount
+#     count = var.workercount
 
-    ami = data.aws_ami.talos.id
-    instance_type = var.instance_type
-    monitoring  = var.nodemonitoringenabled
-    vpc_security_group_ids = [ module.security_group.security_group_id ]
-    subnet_id              = "${element(module.vpc.private_subnets, 0)}"
+#     ami = data.aws_ami.talos.id
+#     instance_type = var.instance_type
+#     monitoring  = var.nodemonitoringenabled
+#     vpc_security_group_ids = [ module.security_group.security_group_id ]
+#     subnet_id              = "${element(module.vpc.private_subnets, 0)}"
 
-    user_data = data.local_file.workerfile.content
-    associate_public_ip_address = true
+#     user_data = data.local_file.workerfile.content
+#     associate_public_ip_address = true
 
-    depends_on = [ data.local_file.workerfile ]
+#     depends_on = [ data.local_file.workerfile ]
 
-    root_block_device {
-       volume_size = 200
-    }
-    tags = {
-       Name = "talosworker"
-    }
+#     root_block_device {
+#        volume_size = 200
+#     }
+#     tags = {
+#        Name = "talosworker"
+#     }
 
 
-}
+# }
 
-resource "aws_ebs_volume" "ebs_volume" {
-  count             = "${var.workercount}"
-  availability_zone = "${element(aws_instance.talos_master_instance.*.availability_zone, count.index)}"
-  size              = "200"
-}
+# resource "aws_ebs_volume" "ebs_volume" {
+#   count             = "${var.workercount}"
+#   availability_zone = "${element(aws_instance.talos_master_instance.*.availability_zone, count.index)}"
+#   size              = "200"
+# }
 
-resource "aws_volume_attachment" "volume_attachement" {
-  count       = "${var.workercount}"
-  volume_id   = "${aws_ebs_volume.ebs_volume.*.id[count.index]}"
-  device_name = "/dev/sdd"
-  instance_id = "${element(aws_instance.talos_worker_instance.*.id, count.index)}"
-}
+# resource "aws_volume_attachment" "volume_attachement" {
+#   count       = "${var.workercount}"
+#   volume_id   = "${aws_ebs_volume.ebs_volume.*.id[count.index]}"
+#   device_name = "/dev/sdd"
+#   instance_id = "${element(aws_instance.talos_worker_instance.*.id, count.index)}"
+# }
 
 resource "aws_lb_target_group" "talos-tg" {
     name = var.talostg
